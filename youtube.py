2f89a4e7ce3b58a8013dcd3f71717f2f06c414ac
Abhishek Dhar <abhishek473@gmail.com>
2022-11-05 17:48:22 -0400

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -11,12 +11,12 @@ def create_task(title, score):
     title = title.replace(' ALBUM REVIEW', '')
     item_content = "[{}] {}".format(score, title)
     project_items = list(filter(lambda x: x['project_id'] == albums_project_id, doist.api.items.all()))
-    filt = filter(lambda x: x['content'] == item_content, project_items)
+    filt = list(filter(lambda x: x['content'] == item_content, project_items))
     print('x-x-x')
     print(albums_project_id)
     print(item_content)
     print(project_items)
-    print(list(filt))
+    print(filt)
     if any(filt):
         return False
     else:
