37e544b5f11ff06f120c4e59dbae3a2906423a53
Abhishek Dhar <abhishek473@gmail.com>
2023-03-19 08:40:49 -0400

    Fixed automations

--- automations.py
+++ automations.py
@@ -1,7 +1,6 @@
-import os
-
 from datetime import datetime, timedelta
 from todoist_api_python.api import TodoistAPI
+from dotenv import dotenv_values
 
 def reset_this_week(api):
     ## Get all tasks associated with the label
@@ -12,13 +11,25 @@ def reset_this_week(api):
 
 def review_tasks(api, today):
     ## Get 5 tasks that do not have a due date and have been created more than 3 months ago
-    tasks = list(filter(lambda x: x.due is None and x.parent_id is None and datetime.strptime(x.created_at.split("T")[0], '%Y-%m-%d') < today - timedelta(days=90), api.get_tasks()))[0:5]
+    def task_filter(task):
+        # Separatedinto a multi-line if statement for readability
+        if 'Review' not in task.labels:
+            if task.due is None:
+                if task.parent_id is None:
+                    if datetime.strptime(task.created_at.split("T")[0], '%Y-%m-%d') < today - timedelta(days=90):
+                        return True
+        return False
+    
+    tasks = list(filter(lambda x: task_filter(x), api.get_tasks()))[0:5]
     for task in tasks:
         task.labels.append('Review')
         api.update_task(task_id=task.id, labels=task.labels)
 
 if __name__ == "__main__":
-    token = os.getenv("TODOIST_API_KEY")
+    secrets = dotenv_values("secrets.env")
+
+    # Retrieve API token from environment variable
+    token = secrets.get("TODOIST_API_KEY")
     
     if not token:
         raise Exception("Please set the TODOIST API token in environment variable.")
@@ -28,8 +39,8 @@ if __name__ == "__main__":
     api = TodoistAPI(token)
 
     # Reset This_Week label if Sunday
-    # if day == 6:
-    #     reset_this_week(api)
+    if day == 6:
+        reset_this_week(api)
 
     # Review tasks every other 
     review_tasks(api, today)
