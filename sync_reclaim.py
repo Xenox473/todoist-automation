7708caf9c7254d25693585ab1d808b02dc5c4bc7
Abhishek Dhar <abhishek473@gmail.com>
2023-06-26 14:36:15 -0400

    Added support for this week labels

--- sync_reclaim.py
+++ sync_reclaim.py
@@ -29,7 +29,7 @@ class SyncReclaim():
                 project_id = list(filter(lambda x: x.id == project_id, self.projects))[0].parent_id
         
     def create_reclaim_task(self, todoist_task, reclaim_task = None):
-        meassage = "Updated" if reclaim_task is not None else "Created"
+        message = "Updated" if reclaim_task is not None else "Created"
         if reclaim_task is None:
             reclaim_task = ReclaimTask()
 
@@ -50,7 +50,7 @@ class SyncReclaim():
         # reclaim_task.min_work_duration = 1
         # reclaim_task.max_work_duration = 2
 
-        reclaim_task.description = todoist_task.id
+        reclaim_task.description = todoist_task.url
         
         reclaim_task.priority = todoist_task.priority
 
@@ -71,31 +71,33 @@ class SyncReclaim():
             reclaim_task.start_date = datetime.now() + timedelta(days = 6 - self.today.weekday()) - timedelta(days=4)
         else:
             # End of the month or end of next month if in last week
-            if self.today.day > 24:
-                reclaim_task.due_date = datetime.now() + timedelta(days = 30 - self.today.day) + timedelta(days=30)
+            if "This_Week" in todoist_task.labels:
+                reclaim_task.start_date = self.today
+                reclaim_task.due_date = datetime.now() + timedelta(days = 6 - self.today.weekday())
             else:
-                reclaim_task.due_date = datetime.now() + timedelta(days = 30 - self.today.day)
+                if self.today.day > 24:
+                    reclaim_task.due_date = datetime.now() + timedelta(days = 30 - self.today.day) + timedelta(days=30)
+                else:
+                    reclaim_task.due_date = datetime.now() + timedelta(days = 30 - self.today.day)
 
             reclaim_task.start_date = datetime.now()
         
         reclaim_task.save()
-        print(meassage + " Reclaim task: " + todoist_task.content + " " + reclaim_task.due_date.strftime("%Y-%m-%d"))
+        print(message + " Reclaim task: " + todoist_task.content + " " + reclaim_task.due_date.strftime("%Y-%m-%d"))
         return True
     
     def grab_todoist_tasks(self):
-        # Grab all tasks that are non recurring tasks and have a due date that is not today
-        tasks = list(filter(lambda x: x.due is not None and x.due.is_recurring is False and x.project_id != self.timeblocking.id, self.todoist_api.get_tasks()))
-        # Grab all tasks that do not have a due date but have a priority
-        tasks += list(filter(lambda x: x.due is None and x.priority > 1 and x.parent_id is None, self.todoist_api.get_tasks()))
+        # Grab all tasks that are labelled "This_Week"
+        tasks = list(filter(lambda x: x.labels is not None and "This_Week" in x.labels, self.todoist_api.get_tasks()))
         return tasks
     
     def cleanup_reclaim_tasks(self, reclaim_tasks, todoist_tasks):
         # Delete tasks that are in Reclaim but not in Todoist
         for task in reclaim_tasks:
-            if task.description not in map(lambda x: x.id, todoist_tasks):
+            if task.description not in map(lambda x: x.url, todoist_tasks):
                 # Mark tasks as completed in Reclaim if they are completed in Todoist
                 try:
-                    todoist_task = self.todoist_api.get_task(task.description)
+                    todoist_task = self.todoist_api.get_task(task.description.split("=")[-1])
                     if todoist_task.is_completed:
                         task.mark_complete()
                         print("Completed Reclaim task: " + task.name)
@@ -111,13 +113,14 @@ class SyncReclaim():
         reclaim_tasks = ReclaimTask.search()
         todoist_tasks = self.grab_todoist_tasks()
 
+        # print(todoist_tasks)
         self.cleanup_reclaim_tasks(reclaim_tasks, todoist_tasks)
 
         for task in todoist_tasks:
-            if task.id not in map(lambda x: x.description, reclaim_tasks):
+            if task.url not in map(lambda x: x.description, reclaim_tasks):
                 self.create_reclaim_task(task)
             else:
-                reclaim_task = list(filter(lambda x: x.description == task.id, reclaim_tasks))[0]
+                reclaim_task = list(filter(lambda x: x.description == task.url, reclaim_tasks))[0]
                 self.create_reclaim_task(task, reclaim_task)
         return
 
